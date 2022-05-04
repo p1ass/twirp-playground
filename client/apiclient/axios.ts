5214e56bfa835d8c1aff1cb7cb6b77f08e13b334
Naoki Kishi <contact@p1ass.com>
2022-05-04 15:14:39 +0900

    format prettier

--- client/apiclient/axios.ts
+++ client/apiclient/axios.ts
@@ -1,27 +1,30 @@
-import axios from 'axios';
-import {Rpc, TwirpError} from "twirp-ts";
+import axios from 'axios'
+import { Rpc, TwirpError } from 'twirp-ts'
 
 const client = axios.create({
-    baseURL: "http://localhost:8080/twirp",
+  baseURL: 'http://localhost:8080/twirp',
 })
 
 // Axiosを使ってリクエストを送る。
 // 200以外のときはTwirpErrorにマッピングを試みる
 export const AxiosRpcClient: Rpc = {
-    request: function (service, method, contentType, data) {
-        return client.post(`${service}/${method}`, data, {
-            responseType: contentType === "application/protobuf" ? 'arraybuffer' : "json",
-            headers: {
-                "content-type": contentType,
-            }
-        }).then(response => {
-            return response.data
-        }).catch(error => {
-            if (error.response && error.response.data) {
-                console.log(error.response)
-                throw TwirpError.fromObject(error.response.data)
-            }
-            throw error
-        })
-    }
+  request: function (service, method, contentType, data) {
+    return client
+      .post(`${service}/${method}`, data, {
+        responseType: contentType === 'application/protobuf' ? 'arraybuffer' : 'json',
+        headers: {
+          'content-type': contentType,
+        },
+      })
+      .then((response) => {
+        return response.data
+      })
+      .catch((error) => {
+        if (error.response && error.response.data) {
+          console.log(error.response)
+          throw TwirpError.fromObject(error.response.data)
+        }
+        throw error
+      })
+  },
 }
--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,68 +1,71 @@
-import type {NextPage} from 'next'
+import type { NextPage } from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
-import {useState} from "react";
-import {UserServiceClientJSON} from "../generated/proto/user/user.twirp";
-import {User} from "../generated/proto/user/user";
-import {AxiosRpcClient} from "../apiclient/axios";
-import {TwirpError} from "twirp-ts";
-import { AuthorizationServiceClientJSON } from '../generated/proto/auth/authorizqation.twirp';
-
+import { useState } from 'react'
+import { UserServiceClientJSON } from '../generated/proto/user/user.twirp'
+import { User } from '../generated/proto/user/user'
+import { AxiosRpcClient } from '../apiclient/axios'
+import { TwirpError } from 'twirp-ts'
+import { AuthorizationServiceClientJSON } from '../generated/proto/auth/authorizqation.twirp'
 
 const Home: NextPage = () => {
-    const [user, setUser] = useState<User | null>(null);
-    
-    const getUser = async () => {
-        const jsonClient = new UserServiceClientJSON(AxiosRpcClient);
-            const res = await jsonClient.GetUser({id: "dummyId"})
-            if (res.user) {
-                setUser(res.user)
-            }
+  const [user, setUser] = useState<User | null>(null)
+
+  const getUser = async () => {
+    const jsonClient = new UserServiceClientJSON(AxiosRpcClient)
+    const res = await jsonClient.GetUser({ id: 'dummyId' })
+    if (res.user) {
+      setUser(res.user)
     }
-    
-    const authorize = async () => {
-        const jsonClient = new AuthorizationServiceClientJSON(AxiosRpcClient);
-        try {
-            await jsonClient.Authorize({})
-        } catch (e) {
-            // これでエラーハンドリングできる
-            if (e instanceof TwirpError) {
-                console.error(e.code)
-                console.error(e.message)
-                console.error(e.meta)
-                alert(e.code+ ": " + e.message)
-            }
-        }
+  }
+
+  const authorize = async () => {
+    const jsonClient = new AuthorizationServiceClientJSON(AxiosRpcClient)
+    try {
+      await jsonClient.Authorize({})
+    } catch (e) {
+      // これでエラーハンドリングできる
+      if (e instanceof TwirpError) {
+        console.error(e.code)
+        console.error(e.message)
+        console.error(e.meta)
+        alert(e.code + ': ' + e.message)
+      }
     }
+  }
 
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Twirp Playground</title>
-                <meta name="description" content="Twirp playground"/>
-                <link rel="icon" href="/favicon.ico"/>
-            </Head>
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Twirp Playground</title>
+        <meta name='description' content='Twirp playground' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
 
-            <main className={styles.main}>
-                <h1 className={styles.title}>
-                    Welcome to <a href="https://github.com/p1ass/twirp-playground">Twirp Playground!</a>
-                </h1>
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a href='https://github.com/p1ass/twirp-playground'>Twirp Playground!</a>
+        </h1>
 
-                <div className={styles.api}>
-                    <h2>Get User API</h2>
-                    <button type='button' onClick={()=> getUser()}>Call GetUser</button>
+        <div className={styles.api}>
+          <h2>Get User API</h2>
+          <button type='button' onClick={() => getUser()}>
+            Call GetUser
+          </button>
 
-                    <p>id: {user?.id}</p>
-                    <p>name: {user?.name}</p>
-                </div>
-
-                <div className={styles.api}>
-                    <h2>Authorize API (Always PermissionDenied Error)</h2>
-                    <button type='button' onClick={()=> authorize()}>Call Authorize</button>
-                </div>
-            </main>
+          <p>id: {user?.id}</p>
+          <p>name: {user?.name}</p>
         </div>
-    )
+
+        <div className={styles.api}>
+          <h2>Authorize API (Always PermissionDenied Error)</h2>
+          <button type='button' onClick={() => authorize()}>
+            Call Authorize
+          </button>
+        </div>
+      </main>
+    </div>
+  )
 }
 
 export default Home
